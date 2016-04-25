276f00427a78ffb379af54b5f4d049e92f542f46
John Stallo <john.stallo@live.com>
2016-04-25 07:42:57 -0700

    clean up

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send('Hello from service B running on ' + os.hostname());
 });
 
 var port = process.env.PORT || 3000;
-app.listen(port, function() {
+app.listen(port, function () {
     console.log("Listening on port " + port);
 });
